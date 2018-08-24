07096692700d25ec3a6e4ccb96a0a3f11e582f60
james <james.c.pino@vanderbilt.edu>
2018-08-24 17:24:05 -0500

    Adding run file to sample various concentrations.

--- run_earm_sample_concentrations.py
+++ run_earm_sample_concentrations.py
@@ -1,26 +1,41 @@
-from pysb.simulator.stochkit import StochKitSimulator
-from pysb.simulator.scipyode import ScipyOdeSimulator
-from pysb.simulator.gpu_ssa import GPUSimulator
-import numpy as np
-from pysb.examples.earm_1_0 import model as earm_model
-import matplotlib.pyplot as plt
-import pandas as pd
-from pysb.logging import setup_logger
 import logging
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from pysb.examples.earm_1_0 import model as earm_model
+from pysb.logging import setup_logger
+from pysb.simulator.gpu_ssa import GPUSimulator
+from pysb.simulator.scipyode import ScipyOdeSimulator
+from pysb.simulator.stochkit import StochKitSimulator
 
 setup_logger(logging.INFO)
 
+# options for observables
+obs = ['cSmac_total', 'tBid_total', 'CPARP_total',
+       'Bid_unbound', 'PARP_unbound', 'mSmac_unbound']
 
-def run_model(param='Bax_0', scale=1.0,  n_sim=1000, simulator='gpu_ssa', precision=np.float32):
-    name = 'cSmac_total'
-    # name = 'tBid_total'
-    tspan = np.linspace(0, 20000, 201)
-    default_value = earm_model.parameters[param].value
-    earm_model.parameters[param].value = default_value * scale
+name = 'cSmac_total'
+# name = 'tBid_total'
+tspan = np.linspace(0, 500, 201)
+
+
+def run_model(ic1=None, factor1=1.0, ic2=None, factor2=1.0,
+              n_sim=5000, simulator='gpu_ssa',
+              precision=np.float64):
+    if ic1 is not None:
+        ic1_default_value = earm_model.parameters[ic1].value
+        earm_model.parameters[ic1].value = ic1_default_value * factor1
+    if ic2 is not None:
+        ic2_default_value = earm_model.parameters[ic2].value
+        earm_model.parameters[ic1].value = ic2_default_value * factor2
 
     if simulator == 'gpu_ssa':
-        sim = GPUSimulator(earm_model, tspan=tspan, verbose=True, precision=precision)
-        traj = sim.run_one_step(tspan=tspan, number_sim=n_sim, threads=32)
+        sim = GPUSimulator(earm_model, tspan=tspan, verbose=True,
+                           precision=precision)
+        # can run with one_step for personal pc. Prevents timeout
+        # traj = sim.run_one_step(tspan=tspan, number_sim=n_sim, threads=32)
+        traj = sim.run(tspan=tspan, number_sim=n_sim, threads=32)
 
     elif simulator == 'stochkit_ssa':
         sim = StochKitSimulator(earm_model, tspan=tspan)
@@ -30,16 +45,28 @@ def run_model(param='Bax_0', scale=1.0,  n_sim=1000, simulator='gpu_ssa', precis
         sim = StochKitSimulator(earm_model, tspan=tspan)
         traj = sim.run(num_processors=8, n_runs=n_sim, tspan=tspan,
                        algorithm='tau_leaping')
-    df = traj.dataframe
-    df.to_csv("earm_{}_{}.csv.gz".format(param, scale), compression='gzip')
-    result = np.array(traj.observables)[name]
 
+    df = traj.dataframe.reset_index()
+    df = df[['simulation', 'time'] + obs]
+    save_name = 'conc_sample'
+
+    if ic1 is not None:
+        save_name += '_{}_{}'.format(ic1, factor1)
+        earm_model.parameters[ic1].value = ic1_default_value
+    if ic2 is not None:
+        save_name += '_{}_{}'.format(ic2, factor2)
+        earm_model.parameters[ic2].value = ic2_default_value
+
+    df.to_csv('{}.csv.gz'.format(save_name), index=False)
+    plot(tspan, traj, name, save_name + '.png')
+
+
+def plot(tspan, traj, obs, save_name, title=None):
+    if title is not None:
+        plt.title(title)
+
+    result = np.array(traj.observables)[obs]
     x = np.array([tr[:] for tr in result]).T
-    save_name = "earm_{}_{}.png".format(param, scale)
-
-    plt.title("Param {} scaled by {}".format(param, scale))
-
-    print("Done plotting {}".format(save_name))
     plt.plot(tspan, x, '0.5', lw=2, alpha=0.25)  # individual trajectories
     plt.plot(tspan, x.mean(axis=1), 'b', lw=3, label='mean')
     plt.plot(tspan, x.max(axis=1), 'b--', lw=2, label="min/max")
@@ -48,7 +75,7 @@ def run_model(param='Bax_0', scale=1.0,  n_sim=1000, simulator='gpu_ssa', precis
     sol = ScipyOdeSimulator(earm_model, tspan)
     traj = sol.run()
 
-    plt.plot(tspan, np.array(traj.observables)[name], label='ode')
+    plt.plot(tspan, np.array(traj.observables)[obs], c='red', label='ode')
 
     plt.legend(loc=0)
 
@@ -56,27 +83,29 @@ def run_model(param='Bax_0', scale=1.0,  n_sim=1000, simulator='gpu_ssa', precis
     print("Done plotting {}".format(save_name))
     # plt.show()
     plt.close()
-    earm_model.parameters[param].value = default_value
+
 
 if __name__ == "__main__":
-    n_sim = 1000
+
+    # number of simulations
+
+    n_sim = 5000
+    run_model(n_sim=100, simulator='gpu_ssa')
+    quit()
+    # does single parameter scan for all initial conditions
     for each in earm_model.initial_conditions:
         print(each[1].name)
-        run_model(each[1].name, scale=0.8, n_sim=n_sim, simulator='gpu_ssa')
-        run_model(each[1].name, scale=1.2, n_sim=n_sim, simulator='gpu_ssa')
-    quit()
+        run_model(each[1].name, factor1=0.8, n_sim=n_sim, simulator='gpu_ssa')
+        run_model(each[1].name, factor1=1.2, n_sim=n_sim, simulator='gpu_ssa')
 
-    run_model('Bax_0', scale=0.8,  n_sim=n_sim, simulator='gpu_ssa')
-    run_model('Bax_0', scale=1.2,  n_sim=n_sim, simulator='gpu_ssa')
-
-    run_model('pC8_0', scale=0.8,  n_sim=n_sim, simulator='gpu_ssa')
-    run_model('pC8_0', scale=1.2,  n_sim=n_sim, simulator='gpu_ssa')
-
-    run_model('Bcl2c_0', scale=0.8,  n_sim=n_sim, simulator='gpu_ssa')
-    run_model('Bcl2c_0', scale=1.2,  n_sim=n_sim, simulator='gpu_ssa')
-
-    run_model('pR_0', scale=0.8,  n_sim=n_sim, simulator='gpu_ssa')
-    run_model('pR_0', scale=1.2,  n_sim=n_sim, simulator='gpu_ssa')
-
-
-
+    for ic1 in earm_model.initial_conditions:
+        name1 = ic1[1].name
+        for ic2 in earm_model.initial_conditions:
+            name2 = ic2[1].name
+            if name1 == name2:
+                continue
+            else:
+                run_model(ic1=name1, factor1=0.8,
+                          ic2=name2, factor2=1.8,
+                          n_sim=n_sim,
+                          simulator='gpu_ssa')
